4a146212685f3df37039f6c3d09772d585f5a4b2
levcoin <34475803+dobrimbozhilov@users.noreply.github.com>
2025-07-15 13:26:37 +0300

    Update cryptonote_basic_impl.cpp - връщане на пре-майна тук

--- src/cryptonote_basic/cryptonote_basic_impl.cpp
+++ src/cryptonote_basic/cryptonote_basic_impl.cpp
@@ -93,6 +93,12 @@ namespace cryptonote {
     const int target_minutes = target / 60;
     const int emission_speed_factor = EMISSION_SPEED_FACTOR_PER_MINUTE - int_log2(target_minutes);    // we assume target_minutes to be power of 2
 
+
+    if (height == 1)
+      {
+        reward = 4500000000000000000; // 4.5M LVC early mining
+        return true;
+      }
     uint64_t base_reward = (MONEY_SUPPLY - already_generated_coins) >> emission_speed_factor;
     if (base_reward < FINAL_SUBSIDY_PER_MINUTE*target_minutes)
     {
