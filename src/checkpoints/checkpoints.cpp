7e14ce2bb81638f5eaf2b2885defe3a2d9ae6ab8
levcoin <34475803+dobrimbozhilov@users.noreply.github.com>
2025-07-17 19:34:02 +0300

    Update checkpoints.cpp - рестарт на чекпойнтите

--- src/checkpoints/checkpoints.cpp
+++ src/checkpoints/checkpoints.cpp
@@ -200,8 +200,8 @@ namespace cryptonote
 
   bool checkpoints::load_checkpoints_from_json(const std::string &json_hashfile_fullpath)
   {
-	  /*  checkpoint loading disabled for LevCoin */
-  /*  boost::system::error_code errcode;
+	  /*  checkpoint loading again enabled for LevCoin */
+    boost::system::error_code errcode;
     if (! (boost::filesystem::exists(json_hashfile_fullpath, errcode)))
     {
       LOG_PRINT_L1("Blockchain checkpoints file not found");
@@ -231,7 +231,7 @@ namespace cryptonote
       }
       ++it;
     } 
-*/
+
 
     return true;
   }
